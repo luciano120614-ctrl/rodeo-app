3e2df2c5c08f1260d0eabf203f130b064b6fabbf
luciano120614-ctrl <luciano120614@gmail.com>
2026-04-16 15:57:59 -0300

    Actualizar tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
   },
   plugins: [],
   safelist: [
-    {pattern: /./}
+    "bg-[#0a1607]","bg-[#0a1207]","bg-[#162208]","bg-[#0d1a08]","bg-[#0a1f07]",
+    "bg-[#142808]","bg-[#1a3a10]","bg-[#1a2e10]","bg-[#1a4a10]","bg-[#2a4a18]",
+    "bg-[#2a5015]","bg-[#3a7a10]","bg-[#4a8a18]","bg-[#5aaa22]","bg-[#0f2a40]",
+    "bg-[#0f1a0a]","bg-[#1a0010]","bg-[#5a0030]","bg-[#7a1a4a]","bg-[#c04080]",
+    "bg-[#4a4a00]","bg-[#1a1a00]","bg-[#0d0d00]","bg-[#b05020]","bg-[#1a72b8]",
+    "bg-[#2a6a10]","bg-[#3d7a10]","bg-[#2a2a00]","bg-[#0e2208]","bg-[#3a6a10]",
+    "bg-[#0a0a00]","bg-[#2a1a08]","bg-[#3a6020]","bg-[#4a4a00]","bg-[#1a1a00]",
+    "border-[#1a2e10]","border-[#2a4a18]","border-[#1e3010]","border-[#5aaa22]",
+    "border-[#7ada3a]","border-[#4a7a28]","border-[#3a1030]","border-[#c04080]",
+    "border-[#e060a0]","border-[#8a8a00]","border-[#4a4a00]","border-[#8a8a28]",
+    "text-[#c8e6a0]","text-[#dff0b0]","text-[#7aaa40]","text-[#a0d060]",
+    "text-[#5a7a30]","text-[#0a2000]","text-[#f0b0d0]","text-[#c070a0]",
+    "text-[#d4d060]","text-[#8a8a40]",
   ]
 }
