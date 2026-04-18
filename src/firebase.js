ef416d59399407a661c5795427076f29c18e727f
luciano120614-ctrl <luciano120614@gmail.com>
2026-04-18 07:57:43 -0300

    Crear firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+// Configuración de Firebase para la app Rodeo
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD02PNtBs0fNC8EDBfxoczMHYrMZEx6QI0",
+  authDomain: "rodeo-app-7e53e.firebaseapp.com",
+  projectId: "rodeo-app-7e53e",
+  storageBucket: "rodeo-app-7e53e.firebasestorage.app",
+  messagingSenderId: "1075119660503",
+  appId: "1:1075119660503:web:621bf4016c74af6bc1bf92"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
