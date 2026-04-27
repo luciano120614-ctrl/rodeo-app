4522061e2837a2697de34ca1f2e28adbb819f9c6
luciano120614-ctrl <luciano120614@gmail.com>
2026-04-27 19:18:29 -0300

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
-
-// ✅ Registro del Service Worker (PWA)
-import { registerSW } from 'vite-plugin-pwa/register'
-
-registerSW()
